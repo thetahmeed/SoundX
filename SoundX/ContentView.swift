e3ff3e4515134170c91aa36cc6dd17461b379b41
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2026-09-14 15:09:42 +0600

    Initial Commit

---
+++ SoundX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundX
+//
+//  Created by Tahmeedul Islam on 14/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundX/SoundXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundXApp.swift
+//  SoundX
+//
+//  Created by Tahmeedul Islam on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
